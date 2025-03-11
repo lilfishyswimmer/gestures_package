c120713a9c233ca687d115885ca8f405da67cfde
lilfishyswimmer <hdemendoza@ucsd.edu>
2025-03-10 18:41:53 -0700

    package checker

---
+++ package_check.py
@@ -0,0 +1,8 @@
+import depthai_hand_tracker
+from depthai_hand_tracker import HandTracker
+from depthai_hand_tracker import HandTrackerRenderer
+
+
+
+print(dir(HandTracker))
+print(dir(HandTrackerRenderer))
